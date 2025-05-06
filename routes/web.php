17529bb63cc6406cc6cf40cd7f3978c81a1f557d
SunnFlower47 <ridwanandrian2004@gmail.com>
2025-05-06 11:41:52 +0700

    update index dan route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use app\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Pertemuan6Controller;
 
+Route::get('/', function () {
+    return redirect('pertemuan6');
+});
 
 Route::resource('pertemuan6', Pertemuan6Controller::class);
 
